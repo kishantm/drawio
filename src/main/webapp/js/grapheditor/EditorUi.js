3cc32586afe06dffc7a9139619d5bcd1442b2bd3
Deep Pancholi <deep.pancholi@threatmodeler.com>
2024-01-09 21:29:45 +0530

    update selection change message

--- src/main/webapp/js/grapheditor/EditorUi.js
+++ src/main/webapp/js/grapheditor/EditorUi.js
@@ -80,7 +80,7 @@ EditorUi = function(editor, container, lightbox)
 		}
 		parent.postMessage(JSON.stringify({
 			event: 'selectionChange',
-			message: { selectionState: { ...sstate, cells: undefined }, isMultipleSelected, selectionType }
+			message: { selectionState: { ...sstate, cells: undefined, vertices: undefined, edges: undefined }, isMultipleSelected, selectionType }
 		}), '*');
 	});
 	
