3db181c616c0688804384279aa7bbfb024f6ab93
PancholiDeep <deep.pancholi@threatmodeler.com>
2024-10-11 18:19:59 +0530

    fix error

--- src/main/webapp/js/grapheditor/EditorUi.js
+++ src/main/webapp/js/grapheditor/EditorUi.js
@@ -88,7 +88,7 @@ EditorUi = function(editor, container, lightbox)
 		}
 		parent.postMessage(JSON.stringify({
 			event: 'selectionChange',
-			message: { selectionState: { ...sstate, cells: sstate.map(c => c.id), vertices: undefined, edges: undefined }, isMultipleSelected, selectionType, selectedCellId, selectedCellMXObjectId, selectedThreatModelerGuid }
+			message: { selectionState: { ...sstate, cells: sstate.cells.map(c => c.id), vertices: undefined, edges: undefined }, isMultipleSelected, selectionType, selectedCellId, selectedCellMXObjectId, selectedThreatModelerGuid }
 		}), '*');
 	});
 	
